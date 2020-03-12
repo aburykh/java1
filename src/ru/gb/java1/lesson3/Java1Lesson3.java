13be362118a37a5f034dacdc322e017d66d1aac1
Aleksey Burykh <burykh@gmail.com>
2020-03-12 06:25:56 +0300

    TicTacToe - update

--- src/ru/gb/java1/lesson3/Java1Lesson3.java
+++ src/ru/gb/java1/lesson3/Java1Lesson3.java
@@ -9,16 +9,17 @@ public class Java1Lesson3 {
     private static final char DOT_AI = 'O';
     private static final char DOT_EMPTY = '_';
 
-    private static int fieldSizeX;
-    private static int fieldSizeY;
+    private static int fieldSizeX = 3;
+    private static int fieldSizeY = 3;
+    private static int toWin = 3;
     private static char[][] field;
 
     private static final Scanner SCANNER = new Scanner(System.in);
     private static final Random RANDOM = new Random();
 
     private static void initField() {
-        fieldSizeY = 3;
-        fieldSizeX = 3;
+//        fieldSizeY = 3;
+//        fieldSizeX = 3;
         field = new char[fieldSizeY][fieldSizeX];
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
@@ -64,8 +65,18 @@ public class Java1Lesson3 {
             y = RANDOM.nextInt(fieldSizeY);
         } while (!isEmptyCell(x, y));
         field[y][x] = DOT_AI;
-
     }
+//    private static void aiTurn() {
+//        if (!checkAITurn()) {
+//            int x, y;
+//            do {
+//                x = RANDOM.nextInt(fieldSizeX) + 1;
+//                y = RANDOM.nextInt(fieldSizeY) + 1;
+//            } while (!isEmptyCell(x, y));
+//            field[y - 1][x - 1] = DOT_AI;
+//        }
+//    }
+
 
     private static boolean isFieldFull() {
         for (int y = 0; y < fieldSizeY; y++) {
@@ -75,28 +86,101 @@ public class Java1Lesson3 {
         }
         return true;
     }
+/*
+1) use the cycles, Luke
+2) Если сделал ДЗ 2-го урока 5* - это хорошо, если нет - посмотри его разбор на третьем уроке, это поможет.
+3) Пролистай чуть наверх, я там тоже спрашивал совета, где-то в час ночи, написал свои мысли. Спойлер: то, как написано, на самом деле не сработает, надо доработать.
+Допустим, есть у меня метод, положим он принимает char и отдает булевое.
+Хочу я в начале объявить переменную, затем стартуют циклы.
+Роятся эти циклы в массиве, роятся, значит, и если находят char, то переменная делает ++
+В конце всей этой каши я говорю: если переменная достигла значения n то true.
+ */
+//    private static boolean checkWin(char c) {
+//        int [][] arr = new int[fieldSizeY][fieldSizeX];
+//        for (int i = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr.length; j++) {
+//                field[i][i] = c;                  // диагональ 1
+//                field[i][arr.length - 1 - i] = c; // горизонталь
+//                field[arr.length - 1 - i][i] = c; // вертикаль
+//                }
+//            //} return true;
+////        } for (int i = 0; i < arr.length; i++) {
+////            arr[i][arr.length - 1 - i] = c;
+////        } for (int i = 0; i < arr.length; i++) {
+////            arr[arr.length - 1 - i][i] = c;
+//        }
+//
+//
+//
+////        for (int y = 0, x = 0; y < fieldSizeY && x < fieldSizeX; x++) {
+////            for (int x = 0; x < fieldSizeX; x++) {
+////            }
+//        //}
+//        return true;
+//    }
 
-    private static boolean checkWin(char c) {
-        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
-        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
-        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+    public static boolean checkWin() {
+        return checkHor() || checkVert() || checkDiag();
+    }
 
-        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
-        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
-        if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
-
-        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
-        if (field[0][2] == c && field[1][1] == c && field[2][0] == c) return true;
+    public static boolean checkHor(int[][] a) {
+        int [][] arr = new int[fieldSizeY][fieldSizeX];
+        for (int i = 0; i < fieldSizeY; i++) {
+            boolean bool = true;
+            for (int j = 1; j < fieldSizeY && bool; j++)
+                bool = arr[i][j] == arr[i][0];
+            if (bool)
+                return true;
+        }
         return false;
     }
 
+    public static boolean checkVert(int[][] a) {
+        int [][] arr = new int[fieldSizeY][fieldSizeX];
+        for (int i = 0; i < fieldSizeX; i++) {
+            boolean bool = true;
+            for (int j = 1; j < fieldSizeX && bool; j++)
+                bool = arr[j][i] == arr[0][i];
+            if (bool)
+                return true;
+        }
+        return false;
+    }
+
+    public static boolean checkDiag(field) {
+        int [][] arr = new int[fieldSizeY][fieldSizeX];
+        boolean bool = true;
+        for (int i = 1; i < fieldSizeX && bool; i++)
+            bool = (arr[i][i] == arr[0][0]);
+        if (bool)
+            return true;
+        bool = true;
+        for (int i = 1; i < fieldSizeX && bool; i++)
+            bool = arr[fieldSizeY - i - 1][i] == arr[fieldSizeY - 1][0];
+        return bool;
+    }
+
+//    private static boolean checkWin(char c) {
+//        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
+//        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
+//        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+//
+//        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
+//        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
+//        if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
+//
+//        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
+//        if (field[0][2] == c && field[1][1] == c && field[2][0] == c) return true;
+//        return false;
+//    }
+
     public static void main(String[] args) {
-//        while (true) {
+        while (true) {
         playOneRound();
-//            System.out.println("Play again?");
-//            if (SCANNER.next().equals("no"))
-//                break;
-//        }
+            System.out.println("Play again?");
+            if (SCANNER.next().equals("no") /*|| SCANNER.next().equals("n")*/)
+                break;
+        }
     }
 
     private static void playOneRound() {
