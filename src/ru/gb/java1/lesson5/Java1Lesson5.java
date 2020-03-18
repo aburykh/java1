83499fcea277f172bf2a10dcb809c2fc57f713a5
Aleksey Burykh <burykh@azurdrive.ru>
2020-03-18 18:46:50 +0300

    home work #5 - completion

--- src/ru/gb/java1/lesson5/Java1Lesson5.java
+++ src/ru/gb/java1/lesson5/Java1Lesson5.java
@@ -4,8 +4,8 @@
  V   В качестве параметра каждому методу передается величина, означающая или длину препятствия (для бега и плавания), или высоту (для прыжков).
  V 3. У каждого животного есть ограничения на действия (бег: кот 200 м., собака 500 м., Лошадь 1500 м., Птица 5 м.,;
  V   прыжок: кот 2 м., собака 0.5 м., Лошадь 3 м., Птица 0.2 м. ; плавание: кот и птица не умеют плавать, собака 10 м., лошадь 100 м.).
- 4. При попытке животного выполнить одно из этих действий, оно должно сообщить результат. (Например, dog1.run(150); -> результат: 'Пёсик пробежал!')
- 5. Добавить животным разброс в ограничениях. То есть у одной собаки ограничение на бег может быть 400 м., у другой 600 м.
+ V 4. При попытке животного выполнить одно из этих действий, оно должно сообщить результат. (Например, dog1.run(150); -> результат: 'Пёсик пробежал!')
+ V 5. Добавить животным разброс в ограничениях. То есть у одной собаки ограничение на бег может быть 400 м., у другой 600 м.
  */
 
 package ru.gb.java1.lesson5;
@@ -16,13 +16,33 @@ public class Java1Lesson5 {
 
         Bird bird = new Bird("Kesha", "blue", 1, 5, 0.2f);
         Cat cat = new Cat("Markiz", "black", 3, 200, 2);
-        Dog dog = new Dog("Cooper", "white", 2, 500, 10, (float) 0.5);
+        Dog dog = new Dog("Cooper", "white", 2, 500, 10, 0.5f);
         Horse horse = new Horse("Mustang", "brown", 4, 1500, 100, 3);
+
+        Westie westie = new Westie("Casper", "white", 5, 650, 3, 0.6f);
+        Yorkshire yorkshire = new Yorkshire("Yalik", "brown", 16, 300, 12, 0.4f);
+        Beagle beagle = new Beagle("Jonny", "black brown", 3, 900, 7, 1.2f);
+
+//        Animal[] zoo = {bird, cat, dog, horse, westie, yorkshire, beagle};
+//        for (int i = 0; i < zoo.length; i++) {
+//            zoo[i].run(550);
+//            System.out.println(zoo[i].getClass().getName());
+//            System.out.println();
+//        }
+
+        Dog[] dogs = {dog, westie, yorkshire, beagle};
+        for (int i = 0; i < dogs.length; i++) {
+            dogs[i].run(550);
+            System.out.println(dogs[i].getClass().getName());
+            System.out.println();
+        }
+
 
         bird.info();
         cat.info();
         dog.info();
         horse.info();
+        //Westie.info();
         System.out.println();
 
         bird.run(5);
